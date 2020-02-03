c71919ad18ef84fae0fa9239f09b0eead6fba791
Unknown <bgcho98@gmail.com>
2020-02-04 00:23:27 +0900

    init

---
+++ src/main/java/com/steps/hoguking/HogukingApplication.java
@@ -0,0 +1,13 @@
+package com.steps.hoguking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogukingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogukingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steps/hoguking/HogukingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steps.hoguking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogukingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
